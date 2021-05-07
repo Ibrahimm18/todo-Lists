30c3bf6bea5fa61d0a61ee0288ea18aaceb75271
Mohammed Ibrahim <8807290200mib@gmail.com>
2021-05-07 21:28:08 +0530

    added first

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
+import {Todo } from "../../model/Todo";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  faTrashAlt = faTrashAlt;
+  todos: Todo[];
+
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos
+    })
+  }
+
+  changeTodoStatus(todo: Todo){
+    this.todoService.changeSatus(todo);
+  }
+  deleteTodo(todo: Todo){
+    this.todoService.deleteTodo(todo);
+  }
+}
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import {of} from "rxjs";
+import {Todo} from "../model/Todo";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todos: Todo[];
+
+  constructor() {
+    this.todos = [
+      {
+        id: "111",
+        title: "Learned C++",
+        isCompleted: true,
+        date: new Date(),
+      },
+      {
+        id: "222",
+        title: "Learned React",
+        isCompleted: true,
+        date: new Date(),
+      },
+      {
+        id: "333",
+        title: "Learned Angular",
+        isCompleted: false,
+        date: new Date(),
+      },
+    ]
+   }
+
+
+   getTodos(){
+     return of(this.todos)
+   }
+   
+   addTodo(todo: Todo){
+   this.todos.push(todo)
+   }
+
+   changeSatus(todo: Todo){
+     this.todos.map( singleTodo => {
+       if(singleTodo.id == todo.id) {
+         todo.isCompleted = !todo.isCompleted;
+       }
+     });
+
+  }
+
+  deleteTodo(todo: Todo){
+    const indexOfTodo = this.todos.findIndex(
+      (currentObj) => currentObj.id === todo.id
+    );
+    this.todos.splice(indexOfTodo,1)
+
+  } 
+
+
+}
